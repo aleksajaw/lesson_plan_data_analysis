9b108ced37e37985b72178376399f1350fac0973
aleksajaw <jaworska.aleksa@gmail.com>
2024-03-18 17:44:49 +0100

    Added date functions to utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,5 +2,20 @@ function isObject(obj) {
     return typeof obj === 'object' && !!obj;
 }
 
+function getNowFormattedDate() {
+    let today = new Date();
+    let YYYY = today.getFullYear();
+    let MM = correctNumNotation(today.getMonth()+1);
+    let DD = correctNumNotation(today.getDate());
+    let HH = correctNumNotation(today.getHours());
+    let mm = correctNumNotation(today.getMinutes());
+    let ss = correctNumNotation(today.getSeconds());
+
+    return `${YYYY + MM + DD + '_' + HH + mm + ss}`;
+}
+
+function correctNumNotation(val) {
+    return val<10 ? `0${val}` : val;
+}
 
 export {isObject};
