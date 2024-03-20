4043b234bafb37180681a5bb95ec805377229367
aleksajaw <jaworska.aleksa@gmail.com>
2024-03-21 00:16:56 +0100

    Added constants.py

---
+++ src/py/constants.py
@@ -0,0 +1,29 @@
+outputsPath = '../outputs/'
+baseNameForLessonsJSON = 'sortedClassesLessonsData.json'
+schoolPlanPage = 'https://zamkowa15.edu.pl/plan/plan.html'
+linksFrameName = 'list'
+planFrameName = 'plan'
+weekDays = ['poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek']
+
+shouldPrintPlanToConsole = {
+    'class': False,
+    'teacher': True,
+    'classroom': True
+}
+shouldWritePlanToTxt = {
+    'class': True,
+    'teacher': True,
+    'classroom': True
+}
+shouldWritePlanToJSON = {
+    'class': True,
+    'teacher': True,
+    'classroom': True
+}
+keysSpacesAmount = {
+    'lessonNr': 3,
+    'lessonHour': 13,
+    'subjectSymbol': 15,
+    'teacherSymbol': 5,
+    'classroomNr': 5
+}
