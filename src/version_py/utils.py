ab4b749fc4366f8ac982850aa5fce0b8be1b9145
aleksajaw <jaworska.aleksa@gmail.com>
2024-10-05 01:26:42 +0200

    Extend utils.py with basic scraping functions

--- src/version_py/utils.py
+++ src/version_py/utils.py
@@ -1,4 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+from constants import *
 
 
 def isType(obj, value):
     return obj.type == value
+
+
+def getResponse(url=''):
+    print('url: ', url)
+    return requests.get(url)
+
+def getSoup(url=planStartUrl):
+    response = getResponse(url)
+    return BeautifulSoup(response.content, 'html.parser') if response else None
+
+def getWithoutLastPart(url=''):
+    url = url or planStartUrl
+    urlParts = url.split('/')
+    slicedUrlParts = urlParts[slice(len(urlParts)-1)]
+    return '/'.join(slicedUrlParts)
+
+def convertToFrameUrl(url='', frameSrc=''):
+    frameUrl = getWithoutLastPart(url) + '/' + frameSrc
+    return frameUrl
+
+def findInFrame(elTag='', elAttr={}, frameName='', url='', findAll=False):
+    url = url or planStartUrl
+    urlSoup = getSoup(url)
+    if urlSoup:
+        foundFrame = urlSoup.find('frame', {'name': frameName})
+        frameSrc = foundFrame['src'] if foundFrame else None
+        frameSoup = getSoup(convertToFrameUrl(url, frameSrc))
+        return frameSoup.find_all(elTag, elAttr) if findAll else frameSoup.find(elTag, elAttr)
+    return None
