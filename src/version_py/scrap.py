aa93f6dc82f11ce82687f493f7b4213f16f48040
aleksajaw <jaworska.aleksa@gmail.com>
2024-10-05 05:48:15 +0200

    Scrap.py: convert data to classesData, add classList, comment some code

--- src/version_py/scrap.py
+++ src/version_py/scrap.py
@@ -7,20 +7,30 @@ baseUrl = getWithoutLastPart(planStartUrl)
 
 listFrameLinks = findInFrame('a', {'target': 'plan'}, 'list', planStartUrl, True) or []
 
-data = []
+classesData = {}
+classList = []
 
 for link in listFrameLinks:
     
+    currClassName = link.get_text(strip=True)
+    classList.append(currClassName)
+
+    print(currClassName,':', convertToFrameUrl(baseUrl, link.get('href')))
+    
     table = findInFrame('table', {'class': 'tabela'}, 'plan', planStartUrl)
 
+    classData = []
     for row in table.find_all('tr'):
         rowData = [cell.get_text(strip=True) for cell in row.find_all(['td', 'th'])]
         if rowData:
-            data.append(rowData)
+            classData.append(rowData)
+
+    classesData[currClassName.replace('/', ' ')] = classData
+
 
 # Convert the list of lists into a DataFrame
-df = pd.DataFrame(data, columns=data[0])
+#df = pd.DataFrame(classesData, columns=classesData[0])
 
 # Export DataFrame to Excel
-excelFile = 'schedule2.xlsx'
-df.to_excel(excelFile, sheet_name='1A', index=False)
+#excelFile = 'schedule2.xlsx'
+#df.to_excel(excelFile, sheet_name='1A', index=False)
