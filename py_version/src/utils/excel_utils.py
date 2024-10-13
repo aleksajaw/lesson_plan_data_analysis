8b92178db3cf9e6f7a0ce90f46a5c20d34129b0f
aleksajaw <jaworska.aleksa@gmail.com>
2024-10-13 20:48:26 +0200

    Move compareAndUpdate to files_utils from excel_utils

--- py_version/src/utils/excel_utils.py
+++ py_version/src/utils/excel_utils.py
@@ -26,25 +26,6 @@ def delDraftIfNecessary(workbook=ExcelWriter.book):
         deleteExcelSheet(workbook, draftSheetName)
 
 
-def compareAndUpdateFile(filePath='',dataToCompare=''):
-    if bool(filePath) and bool(dataToCompare):
-        try:
-            with open(filePath, "r+") as file:
-                if not (file.read()==dataToCompare):
-                    file.seek(0)
-                    file.write(dataToCompare)
-                    # make sure to delete old redundant value
-                    file.truncate()
-                    file.close()
-                    print(f'{filePath} updated with new data.')
-
-        except FileNotFoundError:
-            with open(filePath, 'w') as file:
-              file.write(dataToCompare)
-              file.close()
-              print(f'{filePath} updated with new data.')
-
-
 def writingObjOfDfsToExcel(writer=ExcelWriter, dataToEnter=None, isConverted = True):
     try:
         classDfs = convertToObjOfDfs(dataToEnter) if not isConverted else dataToEnter
--- py_version/src/utils/files_utils.py
+++ py_version/src/utils/files_utils.py
@@ -1,4 +1,24 @@
 import os
 
+
 def doesFileExist(filePath=''):
     return bool (os.path.isfile(filePath))
+
+
+def compareAndUpdateFile(filePath='', dataToCompare=''):
+    if bool(filePath) and bool(dataToCompare):
+        try:
+            with open(filePath, "r+") as file:
+                if not (file.read()==dataToCompare):
+                    file.seek(0)
+                    file.write(dataToCompare)
+                    # make sure to delete old redundant value
+                    file.truncate()
+                    file.close()
+                    print(f'{filePath} updated with new data.')
+
+        except FileNotFoundError:
+            with open(filePath, 'w') as file:
+              file.write(dataToCompare)
+              file.close()
+              print(f'{filePath} updated with new data.')
