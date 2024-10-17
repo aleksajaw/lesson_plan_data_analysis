119bf068b7c1617645406d66ffddff7dc5513049
aleksajaw <jaworska.aleksa@gmail.com>
2024-10-17 17:12:16 +0200

    Add more fns about file names to files_utils

--- py_version/src/utils/files_utils.py
+++ py_version/src/utils/files_utils.py
@@ -1,5 +1,5 @@
 import os
-from constants import scheduleExcelPath
+from constants import scheduleExcelPath, outputsPath
 from openpyxl import load_workbook, Workbook
 from openpyxl.styles import Alignment as openpyxlAlignment
 from openpyxl.utils import column_index_from_string
@@ -119,3 +119,104 @@ def autoFormatExcelFile(workbook=Workbook()):
 
     except Exception as e:
         print('Error while formatting the Excel file:', e)
+
+
+
+def findFileGroup(desiredBase='', desiredExt='', getSplitFileName=False):
+    from collections import defaultdict
+    group = defaultdict(list)
+
+    for fileName in os.listdir(outputsPath):
+        basename, ext = splitFileName(fileName)
+
+        if desiredExt in ('', '*', ext) and desiredBase in ('', '*', basename):
+            el = (basename, ext)   if getSplitFileName   else fileName
+
+            group.append(el)
+
+    return group
+
+
+
+def splitFileName(fileName=''):
+    basename, ext = ('', '')
+
+    if fileName:
+        basename, ext = os.path.splitext(fileName)
+
+    return (basename, ext)
+
+
+
+def findLastFileInGroup(desiredBase='', desiredExt='', getSplitFileName=False):
+    filesList = findFileGroup(desiredBase, desiredExt, getSplitFileName)
+
+    return filesList[-1]   if len(filesList)   else None
+
+
+
+def getFileMarker(fileName='', separator='-'):
+    if separator in fileName:
+        return removeEmptyStrFromArr(fileName.split(separator))[-1]
+
+    else:
+        return fileName
+
+
+
+def createFileName():
+    basicFileName = 'schedule'
+    fileExt = 'xlsx'
+    fileNameParts = findLastFileInGroup(basicFileName, fileExt, True)
+    difference = ''
+    separator = '-'
+
+    # If there aren't any very similar files in the folder,
+    # use the basic file name.
+    finalFileName = basicFileName
+
+    # Otherwise, expand the basic file name.
+    if len(fileNameParts):
+        difference = findFileNameDifference(fileNameParts[0])
+
+        # But still - only if it is very similar file name.
+        # For example basicFileName + separator + 1.fileExt
+        hasSeparator = separator in difference
+        if hasSeparator:
+            tempDifference = getFileMarker(difference, separator)
+            
+            if len(tempDifference) == 1 and type(tempDifference) == int:
+                difference = tempDifference+1
+        
+        else:
+            difference = 1
+
+        finalFileName+= separator + difference
+
+
+    return finalFileName + '.' + fileExt
+
+        
+
+def findFileNameDifference(fileName='', fileNameToBeCompared=''):
+    from excel_utils import convertDigitInStrToInt
+
+    difference = ''
+    nameToRemove=''
+    isReplacementRequired = False
+
+    if fileNameToBeCompared in fileName:
+        isReplacementRequired = True
+
+
+    difference = fileName   if not isReplacementRequired   else fileNameToBeCompared
+    nameToRemove = fileNameToBeCompared   if not isReplacementRequired   else fileName
+
+    difference.replace(nameToRemove, '')
+
+    return convertDigitInStrToInt(difference)
+
+
+
+def removeEmptyStrFromArr(arr=[]):
+    return [el   for el in arr   if el!='']
