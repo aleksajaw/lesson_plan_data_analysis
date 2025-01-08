9de38171c7d0a6ad18b37b8fc0d5da0a0429c3b8
aleksajaw <jaworska.aleksa@gmail.com>
2025-01-08 01:42:37 +0100

    Move to schedule_utils createGroupsInListBy..., concatAndFilterScheduleDataFrames from schedule_creator

--- py_version/src/utils/schedule_utils.py
+++ py_version/src/utils/schedule_utils.py
@@ -1,5 +1,8 @@
-from src.constants import scheduleExcelClassesPath, weekdays, lessonAttrs
+from src.constants import scheduleExcelClassesPath, weekdays, lessonAttrs, timeIndexes
 from src.utils import autoFormatExcelCellSizes, formatCellBackground, formatCellBorder
+import pandas as pd
+import numpy as np
+import re
 from openpyxl import load_workbook, Workbook
 from openpyxl.styles import PatternFill as openpyxlPatternFill
 
@@ -142,3 +145,147 @@ def autoFormatScheduleExcelCellStyles(workbook=Workbook(), excelFilePath=schedul
 
     except Exception as e:
         print('Error while formatting the cell styles in the Excel file:', e)
+
+
+
+def concatAndFilterScheduleDataFrames(el1=None, el2=None):
+    msgText = ''
+
+    try:
+        #el1 = el1.dropna(axis=1, how='all')
+        el2 = el2.dropna(axis=1, how='all')
+        newDf = pd.concat([el1, el2])#.sort_index()
+        rowsFiltered = []
+
+        # iterate through rows (time indexes)
+        for (day, time), singleLessonAttr in newDf.groupby(timeIndexes):
+            singleRow = {}
+            singleRow[timeIndexes[0]] = day
+            singleRow[timeIndexes[1]] = time
+
+            for col in newDf.columns:
+                booleanMask = singleLessonAttr[col] != ''
+                nonEmptyValues = singleLessonAttr[col][booleanMask].dropna().tolist()
+
+                if nonEmptyValues:
+                    for value in nonEmptyValues:
+                        singleRow[col] = value
+
+            rowsFiltered.append(singleRow)
+
+
+        newDfFiltered = pd.DataFrame(rowsFiltered).set_index(keys=newDf.index.names)
+        newDfFiltered = newDfFiltered.reindex(columns=newDf.columns, fill_value=np.nan)
+
+        return newDfFiltered
+    
+    except Exception as e:
+        msgText = f'Error while concatenating Data Frames for Excel worksheet: {e}'
+        
+    if msgText: print(msgText)
+
+
+
+def createGroupsInListByPrefix(data=[], splitDelimeter = '-', replaceDelimeter = '.r'):
+    msgText = ''
+
+    try:
+        # only leave the part before the first '-' and cut '.r' out
+        groupList = [ (str(item).split(splitDelimeter)[0]).replace(replaceDelimeter,'')
+                            if isinstance(item, str)
+                            else item
+                          for item in data ]
+        
+        # group elements by names starting with the same prefix
+        for i in range(2, len(groupList)):
+            gList = groupList
+            if ( all( isinstance(x, str)   for x in [gList[i-1], gList[i]] )
+                  and  gList[i].startswith(gList[i-1]) ):
+                
+                groupList[i] = gList[i-1]
+    
+    except Exception as e:
+        msgText = f'Error while creating groups in list by prefix: {e}'
+    
+    if msgText: print(msgText)
+    
+    return groupList
+
+
+
+def createGroupsInListByFirstLetter(data=[]):
+    msgText = ''
+
+    try:
+        return [item[0]   for item in data]
+    
+    except Exception as e:
+        msgText = f'Error while creating groups in list by first letter: {e}'
+    
+    if msgText: print(msgText)
+
+
+
+def createGroupsInListByNumbers(data=[], optionalPartInNrPrefix='0'):
+    msgText=''
+
+    try:
+        groupsInList = []
+        for item in data:
+            itemStr = str(item)
+            itemLen = len(itemStr)
+            
+            if any(c.isalpha()   for c in itemStr):
+                item = ''.join([c   for c in itemStr if c.isalpha()])
+
+            elif itemStr.isdigit():
+                if itemLen > 1:
+                    item = itemStr[0] + (itemLen-1) * '0'
+              
+                elif 0 < int(item) < 10:
+                    item = 1
+                
+                item = int(item)
+            
+            else:
+                # match e.g. '.9', '_09', '09' or '9'
+                pattern = r'([^a-zA-Z0-9]*' + re.escape(optionalPartInNrPrefix) + r'*[^a-zA-Z0-9]*)\d+'
+                match = re.match(pattern, itemStr)
+                
+                if match:
+                    # '.', '_0', '0'   only if   optionalPartInNrPrefix = 0
+                    item = match.group(1)
+            
+            groupsInList.append(item)
+        
+        return groupsInList
+    
+    except Exception as e:
+        msgText = f'Error while creating groups in list by numbers: {e}'
+    
+    if msgText: print(msgText)
+
+
+
+def createGroupsInListBy(groupName='', data=[]):
+    result = []
+    msgText = ''
+    
+    try:
+        match groupName:
+            case 'subjects':
+                result = createGroupsInListByPrefix(data)
+        
+            case 'teachers':
+                result = createGroupsInListByFirstLetter(data)
+
+            case 'classrooms':
+                result = createGroupsInListByNumbers(data)
+
+
+    except Exception as e:
+        msgText = f'Error while creating groups in list by...: {e}'
+    
+    if msgText: print(msgText)
+
+    return result
