00929c1f6801c824598907d10d40d909dc80f328
aleksajaw <jaworska.aleksa@gmail.com>
2025-01-09 18:29:28 +0100

    Update displaying getTraceback() message

--- py_version/src/utils/error_utils.py
+++ py_version/src/utils/error_utils.py
@@ -7,5 +7,37 @@ def turnOffFutureWarnings():
 def getTraceback(e):
     import traceback
     import re
-    fileName, lineNo, funcName, code = traceback.extract_tb(e.__traceback__)[0]
-    return f'{re.split(r'[\\/]', fileName)[-1]}\n{lineNo} line: {funcName}\n{code}\n'
+    import sys
+
+    msgText=''
+    fileName, lineNr, fnName, code = traceback.extract_tb(e.__traceback__)[0]
+    excType, excValue, excTb = sys.exc_info()
+
+    defaultSpace = '  '
+    lineStarter = f'{re.split(r'[\\/]', fileName)[-1]}, line {lineNr} in{defaultSpace}'
+    lineSpaces = f'{ len(lineStarter) * ' '}'
+
+    errorName = excType.__name__ + f':{defaultSpace}'
+    errorNameLen = len(errorName)
+    fnName = fnName + f':{defaultSpace}'
+    fnNameLen = len(str(fnName))
+
+    if errorNameLen < fnNameLen:
+        diff = fnNameLen-errorNameLen
+        errorName = diff * ' ' + errorName
+    
+    elif errorNameLen > fnNameLen:
+        diff = errorNameLen-fnNameLen
+        fnName = diff * ' ' + fnName
+        
+    msgText += '\n'
+    msgText += f'{lineStarter}'
+    msgText += f'{fnName}'
+    msgText += f'{code}'
+    msgText += f'\n{lineSpaces}'
+    msgText += f'{errorName}'
+    msgText += f'{excValue}'
+    msgText +='\n'
+
+
+    return msgText
