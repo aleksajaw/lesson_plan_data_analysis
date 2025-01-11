cd165bb40be0b2716fa18ce5f80cf363a53120c4
aleksajaw <jaworska.aleksa@gmail.com>
2025-01-11 23:31:21 +0100

    Update getTraceback(e) by adding a list of local variables used during errors

--- py_version/src/utils/error_utils.py
+++ py_version/src/utils/error_utils.py
@@ -8,10 +8,22 @@ def getTraceback(e):
     import traceback
     import re
     import sys
+    import inspect
 
     msgText=''
+    # extract useful data from exception traceback
     fileName, lineNr, fnName, code = traceback.extract_tb(e.__traceback__)[0]
-    excType, excValue, excTb = sys.exc_info()
+    excType, excValue, excTb = sys.exc_info() # exception info
+    # list of tuples, each element representing a frame in the call stack;
+    # each tuple contains information about one level of the function call
+    stack = inspect.stack()
+    previousFrame = stack[1].frame
+    if previousFrame:
+        # local variables from previous frame as reversed list using [::-1]
+        # list sorted in the order of the most recently declared variable
+        localVars = [item   for item in previousFrame.f_locals.items()   if item[0] not in ['e', 'msgText']][::-1]
+    else:
+        localVars = []
 
     defaultSpace = '  '
     lineStarter = f'{re.split(r'[\\/]', fileName)[-1]}, line {lineNr} in{defaultSpace}'
@@ -37,7 +49,13 @@ def getTraceback(e):
     msgText += f'\n{lineSpaces}'
     msgText += f'{errorName}'
     msgText += f'{excValue}'
-    msgText +='\n'
+    msgText += '\n'
+    
+    if localVars:
+        msgText += 'Local variables during the error:\n'
+        for key, val in localVars:
+            msgText += f'{key}: {val}'
+            msgText += '\n'
 
 
     return msgText
