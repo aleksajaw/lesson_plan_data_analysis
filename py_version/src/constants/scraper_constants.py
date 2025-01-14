a2fd55bc53c705d1e94a624b30b0ff41ca4bae06
aleksajaw <jaworska.aleksa@gmail.com>
2025-01-14 15:29:49 +0100

    Update scraper constants

--- py_version/src/constants/scraper_constants.py
+++ py_version/src/constants/scraper_constants.py
@@ -2,18 +2,27 @@
 schoolAndPlanURLs = [ ('https://zamkowa15.edu.pl', '/plan/plan.html'),
                       ('https://www.mechaniksieradz.edu.pl', '/DANE/Plan/Uczen/biezacy/') ]
 
-schoolURL, partPlanURL = schoolAndPlanURLs[0]
+schoolURL, partPlanURL = schoolAndPlanURLs[1]
 planURL = schoolURL + partPlanURL
 
 # frame name attributes
 linksFrameName = 'list'
 planFrameName = 'plan'
 
-# table week days
-weekdays = ['poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek']
+
+from selenium.webdriver.common.by import By
+
+scraperFindKeys = { 'classList': (By.CSS_SELECTOR, 'a[target="plan"]'),
+                    'table': (By.CLASS_NAME, 'tabela'),
+                    'rows': (By.TAG_NAME, 'tr'),
+                    'cols': (By.XPATH, './/td | .//th') }
+
+scraperPresenceLocators = { 'list': (By.NAME, linksFrameName),
+                            'plan': (By.NAME, planFrameName) }
+
 
 # driver constant
-driverLocationStates = ['default', 'list', 'plan']
+driverLocationStates = ['default', linksFrameName, planFrameName]
 
 # VARIABLES FOR EXCEL CREATOR
 #excelEngineName = 'xlsxwriter'
