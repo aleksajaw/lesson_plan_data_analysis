a6411bf3668eecf3972b87e738a22212b05dd2a6
aleksajaw <jaworska.aleksa@gmail.com>
2025-01-21 04:09:17 +0100

    Delete BeautifulSoup4 from requirements.txt. Move bs_utils.py to utils/.old.

--- py_version/src/utils/__init__.py
+++ py_version/src/utils/__init__.py
@@ -5,7 +5,6 @@ sys.path.append(os.path.dirname(__file__))
 
 
 from requests_utils import *
-from bs_utils import *
 from excel_utils import *
 from files_utils import *
 from schedule_utils import *
