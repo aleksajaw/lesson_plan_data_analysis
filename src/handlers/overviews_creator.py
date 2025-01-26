bc9b88d31b2c0209398268b4981230f744a5272f
aleksajaw <jaworska.aleksa@gmail.com>
2025-01-26 02:45:43 +0100

    Major update of overviews_creator.py

--- src/handlers/overviews_creator.py
+++ src/handlers/overviews_creator.py
@@ -1,6 +1,7 @@
 from src.utils.error_utils import handleErrorMsg, getTraceback
-from src.constants.paths_constants import schedulePath, scheduleClassesGroupedDfsJSONPath, scheduleClassroomsGroupedDfsJSONPath, scheduleTeachersGroupedDfsJSONPath, scheduleSubjectsGroupedDfsJSONPath
-from src.utils.excel_utils import convertDfsJSONToObjOfDfs, writerForWriteObjOfDfsToExcel
+from src.constants.paths_constants import schedulePath, allScheduleGroupedDfsJSONPath, allScheduleDfsJSONPath, scheduleFileBaseNames, scheduleFileGroupedBaseNames
+from src.constants.schedule_structures_constants import noGroupMarker, wholeClassGroupName
+from src.utils.excel_utils import convertDfsJSONToObjOfDfs, writerForExcelWorksheetsWithMultipleDfs
 import pandas as pd
 from pandas import  MultiIndex
 import numpy as np
@@ -10,49 +11,69 @@ import os
 
 def createScheduleOverviews():
     msgText=''
+    
     try:
-        a = convertDfsJSONToObjOfDfs(scheduleClassroomsGroupedDfsJSONPath)
-
-        for sheetName, df in a.items():
+        scheduleOverviewBaseNames = scheduleFileBaseNames + scheduleFileGroupedBaseNames[1:]
+        i=-1
+        for scheduleFilePath in ( allScheduleDfsJSONPath + allScheduleGroupedDfsJSONPath ):
+            i=i+1
+            a = convertDfsJSONToObjOfDfs(scheduleFilePath)
             
-            newDf = {}
-            dfColLvl2Names = df.columns.get_level_values(1).unique()
-            
+            aNew = {}
+            aT = {}
 
-            # Iteration through the 2nd level of the Data Frames columns.
-            for colName in dfColLvl2Names:
-                newIndex = []
+            for sheetName, df in a.items():
+                
+                newDf = {}
+                dfColLvl2Names = df.columns.get_level_values(1).unique()
+                
 
-                # Create the proper Series object with the chosen column for the 2nd level of the columns.
-                lvl2ColContent = df.xs(colName, level=1, axis=1).replace('', pd.NA)
-                # Count the value occurrences there.
-                lvl2ColUniqueValCounter = lvl2ColContent.apply(lambda col: col.value_counts())
-                # Convert .values   =>   np.array()
-                #                .flatten()   =>   flat array
-                lvl2ColUniqueValArr = pd.unique( lvl2ColContent.values.flatten() )
+                # Iteration through the 2nd level of the Data Frames columns.
+                for colName in dfColLvl2Names:
+                    newIndex = []
+
+                    # Create the proper Series object with the chosen column for the 2nd level of the columns.
+                    lvl2ColContent = df.xs(colName, level=1, axis=1).astype(str).replace('', pd.NA)
+                    
+                    # Count the value occurrences there.
+                    try:
+                        lvl2ColUniqueValCounter = lvl2ColContent.apply( lambda col: col.dropna().value_counts() ).fillna(0)
+                    except:
+                        lvl2ColUniqueValCounter = lvl2ColContent.apply( lambda col: col.dropna().value_counts() ).fillna(0)
+
+                    lvl2ColUniqueValCounter['Razem'] = lvl2ColUniqueValCounter.sum(axis=1)
+
+                    # Convert .values   =>   np.array()
+                    #                .flatten()   =>   flat array
+                    lvl2ColUniqueValArr = pd.unique( lvl2ColContent.values.flatten() )
 
 
-                # Create a new MultiIndex.
-                for singleElInCol in lvl2ColUniqueValArr:
-                    # Ignore the empty values.
-                    if pd.notna(singleElInCol):
-                        singleElIndexTuple = (colName, singleElInCol)
-                        newIndex.append( singleElIndexTuple)
+                    # Create a new MultiIndex.
+                    for singleElInCol in lvl2ColUniqueValArr:
+                        # Ignore the empty values.
+                        if pd.notna(singleElInCol):
+                            if singleElInCol == noGroupMarker:
+                                singleElInCol = wholeClassGroupName
 
-                newIndex = MultiIndex.from_tuples(newIndex)
+                            singleElIndexTuple = (colName, singleElInCol)
+                            newIndex.append(singleElIndexTuple)
+
+                    newIndex = MultiIndex.from_tuples(newIndex)
 
 
-                # Complete the data and concatenate with the existing data.
-                lvl2ColUniqueValCounter.index = newIndex
-                try:
-                    newDf = pd.concat([newDf, lvl2ColUniqueValCounter])
-                except:
+                    # Complete the data and concatenate with the existing data.
+                    lvl2ColUniqueValCounter.index = newIndex
                     newDf = lvl2ColUniqueValCounter
 
+                    try:
+                        aNew[sheetName].append(newDf)
+                        aT[sheetName].append(newDf.T)
+                    except:
+                        aNew[sheetName] = [newDf]
+                        aT[sheetName] = [newDf.T]
 
-            a[sheetName] = newDf
-
-        writerForWriteObjOfDfsToExcel(os.path.join(schedulePath,'text.xlsx'), a, False)
+            scheduleOverviewFileName = f'{scheduleOverviewBaseNames[i]}_overview.xlsx'
+            writerForExcelWorksheetsWithMultipleDfs( os.path.join(schedulePath, scheduleOverviewFileName), aNew, False, 'rows')
 
 
     except Exception as e:
