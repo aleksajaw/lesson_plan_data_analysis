f6430fbbd099e8152386f33049716411fe6ed9f6
aleksajaw <jaworska.aleksa@gmail.com>
2025-01-28 02:53:13 +0100

    writers_df_utils: Use excelMargin, correct some coords. Comment out part of writerForExcel...MultipleDfs

--- src/utils/writers_df_utils.py
+++ src/utils/writers_df_utils.py
@@ -4,7 +4,7 @@ from src.constants.schedule_structures_constants import excelMargin, excelDistan
 from pandas import ExcelWriter
 import os
 from converters_utils import convertToDf, convertToObjOfDfs, delInvalidChars, convertObjOfDfsToJSON
-from excel_styles_utils import autoFormatScheduleExcel
+from excel_styles_utils import autoFormatScheduleExcel#, autoFormatOverviewExcel
 
 
 
@@ -14,7 +14,7 @@ def writeAsDfToExcelSheet(writer=ExcelWriter, excelFilePath='', sheetName='', da
     try:
         df = dataToEnter   if isConverted   else convertToDf(dataToEnter)
         
-        df.to_excel(writer, sheet_name=delInvalidChars(sheetName), startrow=coords['row'], startcol=coords['col'], merge_cells=True)
+        df.to_excel(writer, sheet_name=delInvalidChars(sheetName), startrow=excelMargin['row']+coords['row'], startcol=excelMargin['col']+coords['col'], merge_cells=True)
 
         if doesWriteMsg:
             msgText = f'\nThe data has been loaded into the {(os.path.splitext(excelFilePath)[1][1:]).upper()} file   {os.path.basename(excelFilePath)}[{sheetName}].'
@@ -71,7 +71,7 @@ def writeExcelWorksheetsWithMultipleDfs(writer=ExcelWriter, excelFilePath='', da
         groupDfs = {el: convertToObjOfDfs(dataToEnter[el])   for el in dataToEnter}   if not isConverted   else dataToEnter
         
         for groupName in groupDfs:
-            coords = excelMargin.copy()
+            coords = {'row':0, 'col':0}
 
             for singleDf in groupDfs[groupName]:
                 
@@ -100,8 +100,8 @@ def writerForExcelWorksheetsWithMultipleDfs(excelFilePath='', objOfMultipleDfs=N
         with ExcelWriter(excelFilePath, mode='w+', engine=excelEngineName) as writer:       
             writeExcelWorksheetsWithMultipleDfs(writer, excelFilePath, objOfMultipleDfs, True, writingDirection)
 
-            if doesNeedFormat:
-                autoFormatScheduleExcel(writer.book, excelFilePath)
+            #if doesNeedFormat:
+                #autoFormatOverwiewExcel(writer.book, excelFilePath)
 
     except Exception as e:
         msgText = handleErrorMsg(f'\nError while loading data into the file {os.path.basename(excelFilePath)}.', getTraceback(e))
