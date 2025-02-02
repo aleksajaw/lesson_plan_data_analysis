aaeaf466c1f8fca48e0375cac248e4091442bdaf
aleksajaw <jaworska.aleksa@gmail.com>
2025-02-02 23:43:09 +0100

    Move some fn() from converters_ to readers_df_utils. Change their names from convert to read...As...

--- src/utils/converters_utils.py
+++ src/utils/converters_utils.py
@@ -72,67 +72,6 @@ def convertObjOfDfsToJSON(dataToConvert=None):
 
 
 
-# EXCEL CONTENT   =>   OBJECT OF DATA FRAMES   =>   JSON
-def convertExcelToDfsJSON(excelFilePath=scheduleClassesExcelPath):
-    from files_utils import doesFileExist
-    dataToConvert = {}
-    msgText = ''
-
-    if doesFileExist(excelFilePath):
-        try:
-            excelData = read_excel( io=excelFilePath, sheet_name=None, engine=excelEngineName, keep_default_na=False,
-                                    header=[excelMargin['row'], excelMargin['row']+1], index_col=[excelMargin['col'], excelMargin['col']+1])
-
-            for sheetName, df in excelData.items():
-                unnamedColIndices = [col   for i, col in enumerate(df.columns)   if 'Unnamed' in str(col[0])]
-                if len(unnamedColIndices):
-                    df = df.drop(unnamedColIndices, axis=1)
-
-                dataToConvert[sheetName] = df.to_json(orient='split')
-
-        except Exception as e:
-            msgText = handleErrorMsg('\nError converting existing schedule Excel file to JSON.', getTraceback(e))
-
-        if msgText: print(msgText)
-
-
-    return json.dumps(dataToConvert, indent=JSONIndentValue)
-
-
-
-# JSON WITH OBJECT OF DATA FRAMES
-#    =>   OBJECT OF DATA FRAMES
-def convertDfsJSONToObjOfDfs(JSONFilePath = ''):
-    msgText=''
-    objOfDfs = {}
-
-    try:
-        with open(JSONFilePath, 'r') as file:
-            objOfDfsTemp = json.load(file)
-            
-            
-        for dfName, dfData in objOfDfsTemp.items():
-          dfData = json.loads(dfData)
-          dfData['index'] = MultiIndex.from_tuples(dfData['index'], names=timeIndexNames)
-
-          try:
-              dfData['columns'] = MultiIndex.from_tuples(dfColWeekDayNamesTuples5el, names=dayAndAttrNames)
-              objOfDfs[dfName] = DataFrame(data=dfData['data'], index=dfData['index'], columns=dfData['columns'])
-
-          except:
-              dfData['columns'] = MultiIndex.from_tuples(dfColWeekDayNamesTuples4el, names=dayAndAttrNames)
-              objOfDfs[dfName] = DataFrame(data=dfData['data'], index=dfData['index'], columns=dfData['columns'])
-
-
-    except Exception as e:
-        msgText = handleErrorMsg('Error while converting JSON file with Data Frames to object with Data Frames.', getTraceback(e))
-    
-    if msgText: print(msgText)
-
-    return objOfDfs
-
-
-
 def correctValsInColsWithNumbers(df=DataFrame, forceCorrect=False):
     if forceCorrect:
         for col in df.columns:
--- src/utils/readers_df_utils.py
+++ src/utils/readers_df_utils.py
@@ -1,9 +1,9 @@
 from error_utils import handleErrorMsg, getTraceback
 from src.constants.paths_constants import scheduleClassesExcelPath
-from src.constants.conversion_constants import excelEngineName
-from src.constants.schedule_structures_constants import excelMargin
-from converters_utils import convertExcelToDfsJSON
-from pandas import read_excel
+from src.constants.conversion_constants import excelEngineName, JSONIndentValue
+from src.constants.schedule_structures_constants import excelMargin, timeIndexNames, dayAndAttrNames, dfColWeekDayNamesTuples5el, dfColWeekDayNamesTuples4el
+from pandas import read_excel, MultiIndex, DataFrame
+import json
 
 
 
@@ -31,3 +31,66 @@ def readExcelFileAsObjOfDfs(excelFilePath=scheduleClassesExcelPath):
 
 
     return dataToConvert
+
+
+
+# JSON WITH OBJECT OF DATA FRAMES
+#    =>   OBJECT OF DATA FRAMES
+def readDfsJSONAsObjOfDfs(JSONFilePath = ''):
+    msgText=''
+    objOfDfs = {}
+
+    try:
+        with open(JSONFilePath, 'r') as file:
+            objOfDfsTemp = json.load(file)
+            
+            
+        for dfName, dfData in objOfDfsTemp.items():
+          dfData = json.loads(dfData)
+          dfData['index'] = MultiIndex.from_tuples(dfData['index'], names=timeIndexNames)
+
+          try:
+              dfData['columns'] = MultiIndex.from_tuples(dfColWeekDayNamesTuples5el, names=dayAndAttrNames)
+              objOfDfs[dfName] = DataFrame(data=dfData['data'], index=dfData['index'], columns=dfData['columns'])
+
+          except:
+              dfData['columns'] = MultiIndex.from_tuples(dfColWeekDayNamesTuples4el, names=dayAndAttrNames)
+              objOfDfs[dfName] = DataFrame(data=dfData['data'], index=dfData['index'], columns=dfData['columns'])
+
+
+    except Exception as e:
+        msgText = handleErrorMsg('Error while reading JSON file with Data Frames as object with Data Frames.', getTraceback(e))
+    
+    if msgText: print(msgText)
+
+    return objOfDfs
+
+
+
+# EXCEL CONTENT
+#    =>   OBJECT OF DATA FRAMES
+#       =>   JSON
+def readExcelAsDfsJSON(excelFilePath=scheduleClassesExcelPath):
+    from files_utils import doesFileExist
+    dataToConvert = {}
+    msgText = ''
+
+    if doesFileExist(excelFilePath):
+        try:
+            excelData = read_excel( io=excelFilePath, sheet_name=None, engine=excelEngineName, keep_default_na=False,
+                                    header=[excelMargin['row'], excelMargin['row']+1], index_col=[excelMargin['col'], excelMargin['col']+1])
+
+            for sheetName, df in excelData.items():
+                unnamedColIndices = [col   for i, col in enumerate(df.columns)   if 'Unnamed' in str(col[0])]
+                if len(unnamedColIndices):
+                    df = df.drop(unnamedColIndices, axis=1)
+
+                dataToConvert[sheetName] = df.to_json(orient='split')
+
+        except Exception as e:
+            msgText = handleErrorMsg('\nError converting existing schedule Excel file to JSON.', getTraceback(e))
+
+        if msgText: print(msgText)
+
+
+    return json.dumps(dataToConvert, indent=JSONIndentValue)
