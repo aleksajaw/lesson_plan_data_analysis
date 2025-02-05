52873efbce002e7b8c4b32d4d96c9448929698c5
aleksajaw <jaworska.aleksa@gmail.com>
2025-02-05 23:11:26 +0100

    overviews_creator: Create a new createScheduleOverviews(), createOverviewsWithResourcesByDays(), ...ByHours()

--- src/handlers/overviews_creator.py
+++ src/handlers/overviews_creator.py
@@ -1,7 +1,7 @@
 from src.utils.error_utils import handleErrorMsg, getTraceback
-from src.constants.paths_constants import allScheduleGroupedDfsJSONPaths, allScheduleDfsJSONPaths, allScheduleOverviewResourcesExcelPaths, allScheduleGroupedOverviewResourcesExcelPaths, allScheduleOverviewResourcesDfsJSONPaths, allScheduleGroupedOverviewResourcesDfsJSONPaths
+from src.constants.paths_constants import allScheduleGroupedDfsJSONPaths, allScheduleDfsJSONPaths, allScheduleOverviewResourcesExcelPaths, allScheduleGroupedOverviewResourcesExcelPaths, allScheduleOverviewResourcesDfsJSONPaths, allScheduleGroupedOverviewResourcesDfsJSONPaths, allScheduleOverviewResourcesByDaysExcelPaths, allScheduleGroupedOverviewResourcesByDaysExcelPaths, allScheduleOverviewResourcesByDaysDfsJSONPaths, allScheduleGroupedOverviewResourcesByDaysDfsJSONPaths,allScheduleOverviewResourcesByHoursExcelPaths, allScheduleGroupedOverviewResourcesByHoursExcelPaths, allScheduleOverviewResourcesByHoursDfsJSONPaths, allScheduleGroupedOverviewResourcesByHoursDfsJSONPaths
 from src.constants.schedule_structures_constants import noGroupMarker, wholeClassGroupName, sumCellsInRowsColName, sumCellsInColsRowName, sumCellsInRowsColName
-from src.utils.converters_utils import divisionResultAsPercentage, convertValToPercentage
+from src.utils.converters_utils import divisionResultAsPercentage#, convertValToPercentage
 from src.utils.readers_df_utils import readDfsJSONAsObjOfDfs
 from src.utils.writers_df_utils import writerForListOfObjsWithMultipleDfsToJSONAndExcel
 import pandas as pd
@@ -12,11 +12,17 @@ import os
 
 
 def createScheduleOverviews():
+  createOverviewsWithResourcesByDays()
+  createOverviewsWithResourcesByHours()    
+
+
+
+def createOverviewsWithResourcesByDays():
     msgText=''
     
     try:
-        overviewExcelPaths = allScheduleOverviewResourcesExcelPaths + allScheduleGroupedOverviewResourcesExcelPaths
-        overviewDfsJSONPaths = allScheduleOverviewResourcesDfsJSONPaths + allScheduleGroupedOverviewResourcesDfsJSONPaths
+        overviewExcelPaths = allScheduleOverviewResourcesByDaysExcelPaths + allScheduleGroupedOverviewResourcesByDaysExcelPaths
+        overviewDfsJSONPaths = allScheduleOverviewResourcesByDaysDfsJSONPaths + allScheduleGroupedOverviewResourcesByDaysDfsJSONPaths
 
         i=-1
         for filePath in ( allScheduleDfsJSONPaths + allScheduleGroupedDfsJSONPaths ):
@@ -70,7 +76,7 @@ def createScheduleOverviews():
                         singleElIndexTuple = (colName, singleElInCol)
                         newIndex.append(singleElIndexTuple)
 
-                    newIndex = MultiIndex.from_tuples(newIndex)
+                    newIndex = MultiIndex.from_tuples(tuples=newIndex, names=['Typ grupy', 'Element grupy'])
 
 
                     # Complete the data and concatenate with the existing data.
@@ -79,10 +85,10 @@ def createScheduleOverviews():
                     uniqueValFromColsLvl2Counter = pd.concat([uniqueValFromColsLvl2Counter, rowForSumEntireCols])
                     
                     tempDf = uniqueValFromColsLvl2Counter
-                    newDf = DataFrame(index=tempDf.index)
+                    newDf = DataFrame(index=tempDf.index).rename_axis(axis=0, mapper=['Typ grupy', 'Element grupy'])
                     
                     overviewColNames = [tempDf.columns.names[0], 'Typ danych']
-                    maxPercentage = convertValToPercentage(1)
+                    maxPercentage = 'nie dotyczy'#convertValToPercentage(1)
                     
                     for col in tempDf.columns:
                         quantityColName = (col, 'Ilość')
@@ -98,7 +104,7 @@ def createScheduleOverviews():
                             #lastCellInCol = newDf.loc[(newDf.index==sumCellsInColsRowName), quantityColName]
                             lastCellInCol = newDf.loc[newDf.index[-1], quantityColName]
 
-                            partOfDayColName = (col, 'Udział w całości dnia')
+                            partOfDayColName = (col, 'Udział w dniu')
 
                             newDf.loc[newDf.index[:-1], partOfDayColName] = divisionResultAsPercentage(colWithoutLastRow, lastCellInCol)
                             newDf.loc[sumCellsInColsRowName2Lvl, partOfDayColName] = maxPercentage   if lastCellInCol   else 'brak zajęć'
@@ -109,7 +115,7 @@ def createScheduleOverviews():
                             sumValCol = tempDf.loc[sumCellsInColsRowName2Lvl, sumCellsInRowsColName]
                         
                         
-                        partOfWeekColName = (col, 'Udział w całości tygodnia')
+                        partOfWeekColName = (col, 'Udział w tygodniu')
                         newDf[partOfWeekColName] = divisionResultAsPercentage(newDf[quantityColName], sumValCol)                    
 
                     #newDf.loc[(newDf.index==sumCellsInColsRowName), partOfWeekColName] = maxPercentage
@@ -122,7 +128,123 @@ def createScheduleOverviews():
                     overviewDfs[sheetName].append(newDf)
 
 
-            writerForListOfObjsWithMultipleDfsToJSONAndExcel(overviewDfsJSONPaths[i], overviewExcelPaths[i], overviewDfs)
+            writerForListOfObjsWithMultipleDfsToJSONAndExcel(overviewDfsJSONPaths[i], overviewExcelPaths[i], overviewDfs, False)
+
+    except Exception as e:
+        msgText = handleErrorMsg('Error while creating the overviews of schedules.', getTraceback(e))
+    
+    if msgText: print(msgText)
+
+
+
+def createOverviewsWithResourcesByHours():
+    msgText=''
+    
+    try:
+        overviewExcelPaths = allScheduleOverviewResourcesByHoursExcelPaths + allScheduleGroupedOverviewResourcesByHoursExcelPaths
+        overviewDfsJSONPaths = allScheduleOverviewResourcesByHoursDfsJSONPaths + allScheduleGroupedOverviewResourcesByHoursDfsJSONPaths
+
+        i=-1
+        for filePath in ( allScheduleDfsJSONPaths + allScheduleGroupedDfsJSONPaths ):
+            i=i+1
+            fileContent = readDfsJSONAsObjOfDfs(filePath)
+            
+            overviewDfs = {}
+
+            for sheetName, df in fileContent.items():
+                
+                newDf = {}
+                dfColLvl2Names = df.columns.get_level_values(1).unique()
+                overviewDfs[sheetName] = []
+
+                # Iteration through the 2nd level of the DataFrames columns.
+                for colName in dfColLvl2Names:
+                    newIndex = []
+
+                    # Create the proper Series object with the chosen column for the 2nd level of the columns.
+                    colLvl2Content = df.xs(key=colName, level=1, axis=1).astype(str).replace('', pd.NA).T
+
+                    # Count the value occurrences there.
+                    uniqueValFromColsLvl2Counter = (colLvl2Content.apply( lambda col: col.dropna().value_counts().astype(int) ).fillna(0)).groupby(level='Nr', axis=1).sum()
+                    
+                    # Create a new col with the sum of values in the row(s).
+                    uniqueValFromColsLvl2Counter[sumCellsInRowsColName] = uniqueValFromColsLvl2Counter.sum(axis=1)
+
+
+                    # Convert .values   =>   np.array()
+                    #                .flatten()   =>   flat array
+                    uniqueValsArrFromColsLvl2 = pd.unique( colLvl2Content.values.flatten() )
+                    uniqueValsArrFromColsLvl2 = [ val   for val in uniqueValsArrFromColsLvl2   if pd.notna(val)]
+
+
+                    # Create a new row with the sum of values in the col.
+                    sumCellsInColsRowName2Lvl = (sumCellsInColsRowName, len(uniqueValsArrFromColsLvl2))
+                    rowForSumEntireCols = uniqueValFromColsLvl2Counter.sum(axis=0).to_frame(name=sumCellsInColsRowName2Lvl).T
+
+
+                    # Create a new MultiIndex for rows.
+                    for singleElInCol in uniqueValsArrFromColsLvl2:
+                        
+                        if singleElInCol.isdigit():
+                            singleElInCol = int(singleElInCol)
+                        
+                        elif singleElInCol == noGroupMarker:
+                            singleElInCol = wholeClassGroupName
+                        
+                        singleElIndexTuple = (colName, singleElInCol)
+                        newIndex.append(singleElIndexTuple)
+
+                    newIndex = MultiIndex.from_tuples(tuples=newIndex, names=['Typ grupy', 'Element grupy'])
+
+
+                    # Complete the data and concatenate with the existing data.
+                    uniqueValFromColsLvl2Counter.index = newIndex
+                    uniqueValFromColsLvl2Counter = uniqueValFromColsLvl2Counter.sort_index()
+                    uniqueValFromColsLvl2Counter = pd.concat([uniqueValFromColsLvl2Counter, rowForSumEntireCols])
+                    
+                    tempDf = uniqueValFromColsLvl2Counter
+                    newDf = DataFrame(index=tempDf.index).rename_axis(axis=0, mapper=['Typ grupy', 'Element grupy'])
+                    
+                    overviewColNames = [tempDf.columns.names[0], 'Typ danych']
+                    maxPercentage = 'nie dotyczy'#convertValToPercentage(1)
+                    
+                    for col in tempDf.columns:
+                        quantityColName = (col, 'Ilość')
+
+                        newDf[quantityColName] = tempDf[col]
+                        newDf.columns = MultiIndex.from_tuples(tuples=newDf.columns, names=overviewColNames)
+
+
+                        if col != sumCellsInRowsColName:
+                            #colWithoutLastRow = newDf.loc[(newDf.index!=sumCellsInColsRowName), quantityColName]
+                            colWithoutLastRow = newDf.loc[newDf.index[:-1], quantityColName]
+                            
+                            #lastCellInCol = newDf.loc[(newDf.index==sumCellsInColsRowName), quantityColName]
+                            lastCellInCol = newDf.loc[newDf.index[-1], quantityColName]
+
+                            partOfDayColName = (col, 'Udział w dniu')
+                            newDf.loc[newDf.index[:-1], partOfDayColName] = divisionResultAsPercentage(colWithoutLastRow, lastCellInCol)
+                            newDf.loc[sumCellsInColsRowName2Lvl, partOfDayColName] = maxPercentage   if lastCellInCol   else 'brak zajęć'
+                            
+                            sumValCol = tempDf[sumCellsInRowsColName]
+                        
+                        else:
+                            sumValCol = tempDf.loc[sumCellsInColsRowName2Lvl, sumCellsInRowsColName]
+                        
+                        
+                        partOfWeekColName = (col, 'Udział w tygodniu')
+                        newDf[partOfWeekColName] = divisionResultAsPercentage(newDf[quantityColName], sumValCol)                    
+
+                    #newDf.loc[(newDf.index==sumCellsInColsRowName), partOfWeekColName] = maxPercentage
+                    newDf.loc[newDf.index[-1], newDf.columns[-1]] = maxPercentage
+
+
+                    #newDf.columns = MultiIndex.from_tuples(newDf.columns)
+
+                    overviewDfs[sheetName].append(newDf)
+
+
+            writerForListOfObjsWithMultipleDfsToJSONAndExcel(overviewDfsJSONPaths[i], overviewExcelPaths[i], overviewDfs, False)
 
     except Exception as e:
         msgText = handleErrorMsg('Error while creating the overviews of schedules.', getTraceback(e))
