88c1a5de3d0da0cd33918cd3d6c9e59f25dc4876
aleksajaw <jaworska.aleksa@gmail.com>
2025-02-16 20:34:53 +0100

    df_utils: Update consts. Add mean fns. Change order of fns.

--- src/utils/df_utils.py
+++ src/utils/df_utils.py
@@ -1,5 +1,5 @@
 from converters_utils import convertDigitInStrToInt
-from constants.overview_constants import sumCellsInRowsColName, sumCellsInColsRowName
+from constants.overview_constants import sumColName, sumRowName, meanRowName, meanColName, notApplicableVal
 from src.constants.schedule_structures_constants import weekdaysCatDtype
 import pandas as pd
 from pandas import MultiIndex
@@ -17,29 +17,15 @@ def createNewMultiIndexWithNewFirstLvl(df, newFirstLvlVal, newLvlName='', isColu
 
 
 
-def createNewMultiIndexForSumRow(indexNames, firstLvlVal, secondLvlVal=sumCellsInColsRowName, thirdLvlVal=''):
+def createNewMultiIndexForSumRow(indexNames, firstLvlVal, secondLvlVal=sumRowName, thirdLvlVal=''):
     return MultiIndex.from_tuples(tuples=[firstLvlVal + (secondLvlVal, thirdLvlVal)], names=indexNames)
 
 
 
-def setNewDfColsFirstLvl(df, newFirstLvlVal):
-    df.columns = df.columns.set_levels([newFirstLvlVal], level=0)
-
-    return df
-
-
-
-def addNewSumColToDf(df, sumColName=sumCellsInRowsColName):
-    df[(df.columns.get_level_values(0)[0], sumColName)] = df.sum(axis=1)
-
-    return df
-
-
-
-def addNewSumRowsToDf(df, isRowIndexFirstLvlADay=True, sumRowName=sumCellsInColsRowName):
-    newMultiIndexRows = MultiIndex.from_tuples([(day, sumRowName, '')   for day in df.index.get_level_values(0).unique()], names=df.index.names)
-    sumRows = pd.DataFrame(data=[[np.nan] * len(df.columns)], index=newMultiIndexRows, columns=df.columns)
-    df = pd.concat([df, sumRows])
+def addNewCalcRowsToDf(df, calcRowName, lastIndexLvl='', isRowIndexFirstLvlADay=True):
+    newMultiIndexRows = MultiIndex.from_tuples([(day, calcRowName, lastIndexLvl)   for day in df.index.get_level_values(0).unique()], names=df.index.names)
+    calcRows = pd.DataFrame(data=[[np.nan] * len(df.columns)], index=newMultiIndexRows, columns=df.columns)
+    df = pd.concat([df, calcRows])
 
     if isRowIndexFirstLvlADay:
         df = setDfLvlAsType(df, weekdaysCatDtype, sortingType='index', lvlNr=0)
@@ -48,7 +34,54 @@ def addNewSumRowsToDf(df, isRowIndexFirstLvlADay=True, sumRowName=sumCellsInCols
 
 
 
-def setGroupCounterInDfSumRowIndex(df, sumRowName=sumCellsInColsRowName):
+def addNewSumColToDf(df, sumColName=sumColName):
+    #excludedIndices = [meanRowName]
+    sumColTuple = (df.columns.get_level_values(0)[0], sumColName)
+    meanColTuple = (df.index.get_level_values(0)[0], meanColName)
+
+    df[sumColTuple] = df.sum(axis=1)
+
+    df.loc[meanColTuple, sumColTuple] = notApplicableVal
+
+    return df
+
+
+
+def addNewMeanColToDf(df, meanColName=meanColName):
+    #excludedIndices = [sumRowName, meanRowName]
+    excludedCols = [sumColName]
+    meanColTuple = (df.columns.get_level_values(0)[0], meanColName)
+
+    df[meanColTuple] = ( df.drop(columns=excludedCols, level=1, axis=1).fillna(0.0)
+                           .mean(axis=1).round(2) )
+
+    return df
+
+
+
+def setNewDfColsTitle(df, newFirstLvlVal):
+    df.columns = df.columns.set_levels([newFirstLvlVal], level=0)
+
+    return df
+
+
+
+# Making the 1st lvl a concrete data type (for example CategoricalDType) to simplify the sorting proccess.
+def setDfLvlAsType(df, dataType, sortingType='', lvlNr=0, isIndex=True):
+    if isIndex:
+        df.index = df.index.set_levels( df.index.levels[0].astype(dataType), level=lvlNr)
+
+        if sortingType=='index':
+            df = df.sort_index()
+
+        elif sortingType=='values':
+            df = df.sort_values(df.index.names[:-1])
+
+    return df
+
+
+
+def setGroupCounterInDfSumRowIndex(df, sumRowName=sumRowName):
     counterList = []
 
     for groupName in df.index.get_level_values(0).unique():
@@ -73,22 +106,71 @@ def setGroupCounterInDfSumRowIndex(df, sumRowName=sumCellsInColsRowName):
 
 
 
-def countNonEmptyRowsInGroup(df, groupName):
-    isRowEmptyMask = df.isna().all(axis=1) | (df == '').all(axis=1)
-    return df[~(isRowEmptyMask)].loc[groupName][:-1].sum(axis=1).gt(0).sum()
+def getDfValidIndices(df):
+    return (df.first_valid_index(), df.last_valid_index())
 
 
 
-def writeDfColSumToCell(df, keySum, col, newVal=None, firstValidIndex=None, lastValidIndex=None, firstLvlRowMultiIndex=None):
+def writeDfColMeanToCell(df, meanKey, col, newVal=None, firstValidIndex=None, lastValidIndex=None, firstLvlRowMultiIndex=None):
+    excludedIndices = [sumRowName, meanRowName]
+    
     if newVal is not None:
-        df.loc[keySum, col] = newVal
+        df.loc[meanKey, col] = newVal
 
     elif firstValidIndex is not None   and   lastValidIndex is not None:
-        df.loc[keySum, col] = df.loc[firstValidIndex:lastValidIndex, col].sum(skipna=True)
+        df.loc[meanKey, col] = df.loc[firstValidIndex:lastValidIndex, col].fillna(0.0).mean().round(2)
         
     elif firstLvlRowMultiIndex is not None:
-        df.loc[keySum, col] = df.loc[firstLvlRowMultiIndex, col][:-1].sum(skipna=True)
+        df.loc[meanKey, col] = df.loc[firstLvlRowMultiIndex, col].drop(index=excludedIndices).fillna(0.0).mean().round(2)
     
+    return df
+
+
+
+def writeDfColSumToCell(df, sumKey, col, newVal=None, firstValidIndex=None, lastValidIndex=None, firstLvlRowMultiIndex=None):
+    excludedIndices = [sumRowName, meanRowName]
+
+    if newVal is not None:
+        df.loc[sumKey, col] = newVal
+
+    elif firstValidIndex is not None   and   lastValidIndex is not None:
+        df.loc[sumKey, col] = df.loc[firstValidIndex:lastValidIndex, col].sum(skipna=True)
+        
+    elif firstLvlRowMultiIndex is not None:
+        
+        df.loc[sumKey, col] = df.loc[firstLvlRowMultiIndex, col].drop(index=excludedIndices).fillna(0.0).sum(skipna=True)
+    
+    return df
+
+
+
+# Completely transform DataFrame into a vertical order where column names become the 1st level of the MultiIndex for the rows.
+def completelyTransformDfToVerticalOrder(df, dfName, newDfLvlName):
+    df = df.stack( level=0, dropna=False)
+    
+    # Correct the order of the levels in the hierarchy.
+    df = reorderDfLvls(df, newOrder=[2,0,1], isRowIndexFirstLvlADay=True)
+    
+    # Add the parent value for the class columns.
+    df.columns = createNewMultiIndexWithNewFirstLvl(df, dfName, newDfLvlName.capitalize())
+
+    #df = removeDfEmptyRows(df)
+    df = removeDuplicatedDfRows(df)
+    return df
+
+
+
+# Correct the order of the levels in the hierarchy.
+def reorderDfLvls(df, newOrder, isIndex=True, isRowIndexFirstLvlADay=True):
+    if isIndex:
+        df.index = df.index.reorder_levels( newOrder )
+
+    else:
+        df.columns = df.columns.reorder_levels( newOrder )
+
+    if isRowIndexFirstLvlADay:
+        df = setDfLvlAsType(df, weekdaysCatDtype, 'values')
+
     return df
 
 
@@ -131,15 +213,18 @@ def removeDuplicatedDfRows(df, keepType='first', checkEmptiness=True):
 
 
 
-def convertDfValsToCounters(df, lvlList=[0], retainOnlyLastRows=True):
-    df = ( df.map(lambda val: 1   if val   else np.nan)
-             .groupby(level=lvlList, sort=False)
-             .cumsum() )
-    
-    if retainOnlyLastRows:
-        df = removeDuplicatedDfRows(df, keepType='last', checkEmptiness=False)
-    
+def retainOnlyFirstCellsInDfGroups(df, axisNr=1, lvlNr=0):
+    df = df.groupby(axis=axisNr, level=lvlNr, sort=False).first()
+
     return df
+
+
+
+def countNonEmptyRowsInGroup(df, groupName):
+    isRowEmptyMask = df.isna().all(axis=1) | (df == '').all(axis=1)
+    excludedIndices = [sumRowName, meanRowName]
+
+    return df[~(isRowEmptyMask)].loc[groupName].drop(index=excludedIndices).sum(axis=1).gt(0).sum()
 
 
 
@@ -153,58 +238,12 @@ def convertDfValsToBinaryStates(df, useInt=True, useNan=True):
 
 
 
-def retainOnlyFirstCellsInDfGroups(df, axisNr=1, lvlNr=0):
-    df = df.groupby(axis=axisNr, level=lvlNr, sort=False).first()
-
-    return df
-
-
-
-def getDfValidIndices(df):
-    return (df.first_valid_index(), df.last_valid_index())
-
-
-
-# Correct the order of the levels in the hierarchy.
-def reorderDfLvls(df, newOrder, isIndex=True, isRowIndexFirstLvlADay=True):
-    if isIndex:
-        df.index = df.index.reorder_levels( newOrder )
-
-    else:
-        df.columns = df.columns.reorder_levels( newOrder )
-
-    if isRowIndexFirstLvlADay:
-        df = setDfLvlAsType(df, weekdaysCatDtype, 'values')
-
-    return df
-
-
-
-# Making the 1st lvl a concrete data type (for example CategoricalDType) to simplify the sorting proccess.
-def setDfLvlAsType(df, dataType, sortingType='', lvlNr=0, isIndex=True):
-    if isIndex:
-        df.index = df.index.set_levels( df.index.levels[0].astype(dataType), level=lvlNr)
-
-        if sortingType=='index':
-            df = df.sort_index()
-
-        elif sortingType=='values':
-            df = df.sort_values(df.index.names[:-1])
-
-    return df
-
-
-
-# Completely transform DataFrame into a vertical order where column names become the 1st level of the MultiIndex for the rows.
-def completelyTransformDfToVerticalOrder(df, dfName, newDfLvlName):
-    df = df.stack( level=0, dropna=False)
+def convertDfValsToCounters(df, lvlList=[0], retainOnlyLastRows=True):
+    df = ( df.map(lambda val: 1   if val   else np.nan)
+             .groupby(level=lvlList, sort=False)
+             .cumsum() )
     
-    # Correct the order of the levels in the hierarchy.
-    df = reorderDfLvls(df, newOrder=[2,0,1], isRowIndexFirstLvlADay=True)
+    if retainOnlyLastRows:
+        df = removeDuplicatedDfRows(df, keepType='last', checkEmptiness=False)
     
-    # Add the parent value for the class columns.
-    df.columns = createNewMultiIndexWithNewFirstLvl(df, dfName, newDfLvlName.capitalize())
-
-    #df = removeDfEmptyRows(df)
-    df = removeDuplicatedDfRows(df)
     return df
