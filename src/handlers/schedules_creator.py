a8bc2d7055ae8475a7e92786feed7edc62669076
aleksajaw <jaworska.aleksa@gmail.com>
2025-02-18 20:48:21 +0100

    schedules_creator: Add try...except blocks. Simplify values of vars where possible.

--- src/handlers/schedules_creator.py
+++ src/handlers/schedules_creator.py
@@ -156,8 +156,6 @@ def fullConstructAndWriteScheduleByGroup(ownersType, schedulesObj, dfsJSONFilePa
 
     schedulesByGroups = {}
     try:
-        #if schedulesObj is None:
-        #    schedulesObj = {}
         schedulesByGroups = concatAndFilterSingleGroupListDataFrames(ownersType, schedulesObj, groupedOwnerLists[ownersType])
 
         writerForObjOfDfsToJSONAndExcel(dfsJSONFilePath, excelFilePath, schedulesByGroups)
@@ -173,138 +171,152 @@ def fullConstructAndWriteScheduleByGroup(ownersType, schedulesObj, dfsJSONFilePa
 
 
 def buildOwnersTypeScheduleBasedOnCol(targetDict, baseDf, ownersType, newColValue, newMainColKey='class'):
+    msgText=''
     #if targetDict is None:
     #    targetDict = {}
     #if baseDf is None:
     #    baseDf = DataFrame
     
-    # column name in timetable to be changed
-    colToBeChanged = getTranslByPlural(ownersType)
+    try:
+        # column name in timetable to be changed
+        colToBeChanged = getTranslByPlural(ownersType)
 
-    # make life (program) easier to understand:)
-    newScheduleOwner = colToBeChanged
+        # make life (program) easier to understand:)
+        newScheduleOwner = colToBeChanged
 
-    # main column in basic schedule is subject
-    newMainColName = getTranslation(newMainColKey)
-            
-    # get the list of teachers/classrooms/subjects inside the timetable
-    group = ( baseDf.xs(newScheduleOwner, axis=1, level=1)
-                    .stack(sort=False)
-                    .unique() )
-    group = filterNumpyNdarray(group)
+        # main column in basic schedule is subject
+        newMainColName = getTranslation(newMainColKey)
+                
+        # get the list of teachers/classrooms/subjects inside the timetable
+        group = ( baseDf.xs(newScheduleOwner, axis=1, level=1)
+                        .stack(sort=False)
+                        .unique() )
+        group = filterNumpyNdarray(group)
 
-    if len(group):
+        if len(group):
 
-        # loop for each teacher/classroom/subject
-        for el in group:
+            # loop for each teacher/classroom/subject
+            for el in group:
 
-            # copy for modification
-            baseDfCopy = baseDf.copy().astype('object')
-            # axis  =1 => columns
-            #       =0 => index
-            #
-            # level of MultiIndex in column
-            # level =1 => weekdays
-            #       =2 => subject, teacher, classroom
-            newScheduleOwnerTempData = baseDfCopy.xs(newScheduleOwner, axis=1, level=1)
-            
-            # get cells with value of specific el in group (teacher, classroom or subject)
-            # e.g. KJ, 110, mat
-            # not whole teacher, classroom, subject
-            #
-            # use data type (integer or other)
-            try:
-                maskToFindDesiredPartOfLessons = newScheduleOwnerTempData == int(el)
-            except:
-                maskToFindDesiredPartOfLessons = newScheduleOwnerTempData == el
+                # axis  =1 => columns
+                #       =0 => index
+                #
+                # level of MultiIndex in column
+                # level =1 => weekdays
+                #       =2 => subject, teacher, classroom
+                newScheduleOwnerTempData = baseDf.xs(newScheduleOwner, axis=1, level=1)
+                
+                # get cells with value of specific el in group (teacher, classroom or subject)
+                # e.g. KJ, 110, mat
+                # not whole teacher, classroom, subject
+                #
+                # use data type (integer or other)
+                try:
+                    maskToFindDesiredPartOfLessons = newScheduleOwnerTempData == int(el)
+                except:
+                    maskToFindDesiredPartOfLessons = newScheduleOwnerTempData == el
 
-            emptyValue = np.nan
+                emptyValue = np.nan
 
-            # use mask to get whole desired lessons
-            # replace not matching whole rows with emptyValue
-            # leave rows with any matches untouched
-            elRows = baseDfCopy.where(maskToFindDesiredPartOfLessons.any(axis=1), emptyValue)
+                # use mask to get whole desired lessons
+                # replace not matching whole rows with emptyValue
+                # leave rows with any matches untouched
+                elRows = baseDf.where(maskToFindDesiredPartOfLessons.any(axis=1), emptyValue)
 
-            # divide rows into weekdays
-            # replace value with NaN for not matching days
-            for day in weekdays:
-                # divide earlier mask to days 
-                elDayMask = maskToFindDesiredPartOfLessons[day]
+                # divide rows into weekdays
+                # replace value with NaN for not matching days
+                for day in weekdays:
+                    # divide earlier mask to days 
+                    elDayMask = maskToFindDesiredPartOfLessons[day]
 
-                # go through each day,
-                # ignore other days to make things easier
-                # replace not matching day cells with emptyValue
-                elRows[day] = elRows[day].where(elDayMask, emptyValue)
+                    # go through each day,
+                    # ignore other days to make things easier
+                    # replace not matching day cells with emptyValue
+                    elRows[day] = elRows[day].where(elDayMask, emptyValue)
 
 
-            # restrict loop actions to rows which match mask
-            if not elRows.empty:
+                # restrict loop actions to rows which match mask
+                if not elRows.empty:
 
-                # change the column name and its value
-                elRows = elRows.rename( columns={ colToBeChanged: newMainColName } )
+                    # change the column name and its value
+                    elRows = elRows.rename( columns={ colToBeChanged: newMainColName } )
 
-                # asign
-                #   to the column "klasa" (old "nauczyciel", "przedmiot", "sala")
-                #   mapped values of the column "klasa" to className
-                # only if current value is not NaN in numeric arrays,
-                #   None or NaN in object arrays,
-                #   NaT in datetimelike
-                elRows.loc[:,(weekdays, newMainColName)] = ( elRows.loc[:, (weekdays, newMainColName)]
-                                                                   .map( lambda x: newColValue   if pd.notna(x)
-                                                                                                 else x ) )
+                    # asign
+                    #   to the column "klasa" (old "nauczyciel", "przedmiot", "sala")
+                    #   mapped values of the column "klasa" to className
+                    # only if current value is not NaN in numeric arrays,
+                    #   None or NaN in object arrays,
+                    #   NaT in datetimelike
+                    elRows.loc[:,(weekdays, newMainColName)] = ( elRows.loc[:, (weekdays, newMainColName)]
+                                                                      .map( lambda x: newColValue   if pd.notna(x)
+                                                                                                    else x ) )
 
-                elRows = ( elRows.groupby(level=[0,1], sort=False)
-                                 .first() )
-                #indexLength = len(elRows.index)
-                #elRows.insert(loc=0, column=timeIndexNames[1], value=lessonTimePeriods[:indexLength])
+                    elRows = ( elRows.groupby(level=[0,1], sort=False)
+                                    .first() )
+                    #indexLength = len(elRows.index)
+                    #elRows.insert(loc=0, column=timeIndexNames[1], value=lessonTimePeriods[:indexLength])
 
-                if el not in targetDict:
-                    targetDict[str(el)] = elRows
+                    if el not in targetDict:
+                        targetDict[str(el)] = elRows
 
-                else:
-                    x = targetDict[str(el)].copy()
-                    #y = x.combine_first(elRows)
-                    #y = x.join(elRows)
+                    else:
+                        x = targetDict[str(el)].copy()
+                        #y = x.combine_first(elRows)
+                        #y = x.join(elRows)
 
-                    targetDict[str(el)] = concatAndFilterScheduleDataFrames(x, elRows)
+                        targetDict[str(el)] = concatAndFilterScheduleDataFrames(x, elRows)
+
+    except Exception as e:
+        msgText = handleErrorMsg(f'\nError while building the {ownersType} schedule based on the DataFrame column.', getTraceback(e))
+    
+    if msgText: print(msgText)
 
 
 
 def createScheduleGroupedOwnerObjOfDfs(dataToEnter):
+    msgText = ''
     #if dataToEnter is None:
     #    dataToEnter = {}
-    
-    dataToEnterSorted = sortObjKeys(dataToEnter)
-    sheetsData = { sheetName: dataToEnterSorted[sheetName]   for sheetName in dataToEnterSorted.keys() }
+
     objOfDfs = {}
-
-    # basic structure for the group list sheets
-    for sheetName, sheetData in sheetsData.items():
-
-        namesBaseList = createGroupsInListBy(sheetName, sheetData)
-        dfBase = { 'names_base':  namesBaseList,
-                   'names'     :  sheetData }
+    try:
         
-        objOfDfs[sheetName] = DataFrame(dfBase)
-        objOfDfs[sheetName]['names_No.'] = RangeIndex(start=1, stop=len(objOfDfs[sheetName])+1, step=1)
+        dataToEnterSorted = sortObjKeys(dataToEnter)
+        sheetsData = { sheetName: dataToEnterSorted[sheetName]   for sheetName in dataToEnterSorted.keys() }
+
+
+        # basic structure for the group list sheets
+        for sheetName, sheetData in sheetsData.items():
+
+            namesBaseList = createGroupsInListBy(sheetName, sheetData)
+            dfBase = { 'names_base':  namesBaseList,
+                      'names'     :  sheetData }
+            
+            objOfDfs[sheetName] = DataFrame(dfBase)
+            objOfDfs[sheetName]['names_No.'] = RangeIndex(start=1, stop=len(objOfDfs[sheetName])+1, step=1)
+        
+
+        # develop the structure of the worksheet objects
+        for listName in objOfDfs:
+            df = objOfDfs[listName]
+
+            # indices & their columns
+            df['group_No.']          = ( (df.groupby('names_base', sort=False)
+                                            .ngroup() + 1)
+                                            .astype(str) + '.' )
+            
+            df['names_in_group_No.'] = ( (df.groupby('names_base', sort=False)
+                                            .cumcount() + 1)
+                                            .astype(str) + '.' )
+            
+            df.set_index(keys=['group_No.', 'names_base', 'names_in_group_No.'], inplace=True)
+            objOfDfs[listName] = df
+
+
+    except Exception as e:
+        msgText = handleErrorMsg(f'\nError while creating a DataFrame object with grouped schedule owners.', getTraceback(e))
     
-
-    # develop the structure of the worksheet objects
-    for listName in objOfDfs:
-        df = objOfDfs[listName]
-
-        # indices & their columns
-        df['group_No.']          = ( (df.groupby('names_base', sort=False)
-                                        .ngroup() + 1)
-                                        .astype(str) + '.' )
-        
-        df['names_in_group_No.'] = ( (df.groupby('names_base', sort=False)
-                                        .cumcount() + 1)
-                                        .astype(str) + '.' )
-        
-        df.set_index(keys=['group_No.', 'names_base', 'names_in_group_No.'], inplace=True)
-        objOfDfs[listName] = df
-
+    if msgText: print(msgText)
 
     return objOfDfs
 
@@ -392,41 +404,51 @@ def writeGroupListsToExcelAndFormat(objOfDfs):
 
 
 def concatAndFilterSingleGroupListDataFrames(ownersType, sheetsForOwnerTypes, ownersList, addNewCol=True):
+    msgText=''
+    
     #if sheetsForOwnerTypes is None:
     #    sheetsForOwnerTypes = {}
     #if ownersList is None:
     #    ownersList = DataFrame
     objOfDfs = {}
-    newColName = getTranslByPlural(ownersType)
-    
-    ownersPureGroupedList = getPureGroupedList(ownersList)
 
-    # get group names like 'ang', '100' etc.
-    for groupName, groupList in ownersPureGroupedList.items():
-        
-        # get elements like 'ang.r', '101' etc.
-        for el in groupList:
-            x = objOfDfs[str(groupName)]   if str(groupName) in objOfDfs.keys()   else None
-            y = sheetsForOwnerTypes[str(el)].copy()
+    try:
+        newColName = getTranslByPlural(ownersType)
+        ownersPureGroupedList = getPureGroupedList(ownersList)
 
-            if isinstance(x, DataFrame):
-                # ... and concatenate them :)
-                # the line below prevents FutureWarning:
-                #   The behavior of DataFrame concatenation with empty or all-NA entries is deprecated.
-                #   In a future version, this will no longer exclude empty or all-NA columns when determining the result dtypes.
-                #   To retain the old behavior, exclude the relevant entries before the concat operation.
-                x = dropnaInDfByAxis(x, 1)
-                #y = dropnaInDfByAxis(y, 1)
-                if addNewCol:
-                    objOfDfs[str(groupName)] = concatAndFilterScheduleDataFrames(x, y, True, newColName, str(el))
-                else:
-                    objOfDfs[str(groupName)] = concatAndFilterScheduleDataFrames(x, y)
+        # get group names like 'ang', '100' etc.
+        for groupName, groupList in ownersPureGroupedList.items():
+            
+            if not isinstance(groupName, str):
+                groupName = str(groupName)
+
+            # get elements like 'ang.r', '101' etc.
+            for el in groupList:
+                x = objOfDfs[groupName]   if groupName in objOfDfs.keys()   else None
+                y = sheetsForOwnerTypes[str(el)].copy()
+
+                if isinstance(x, DataFrame):
+                    # ... and concatenate them :)
+                    # the line below prevents FutureWarning:
+                    #   The behavior of DataFrame concatenation with empty or all-NA entries is deprecated.
+                    #   In a future version, this will no longer exclude empty or all-NA columns when determining the result dtypes.
+                    #   To retain the old behavior, exclude the relevant entries before the concat operation.
+                    x = dropnaInDfByAxis(x, 1)
+                    #y = dropnaInDfByAxis(y, 1)
+                    if addNewCol:
+                        objOfDfs[groupName] = concatAndFilterScheduleDataFrames(x, y, True, newColName, el)
+                    else:
+                        objOfDfs[groupName] = concatAndFilterScheduleDataFrames(x, y)
                     
-            else:
-                if addNewCol:
-                    objOfDfs[str(groupName)] = filterAndConvertScheduleDataFrames(y, True, newColName, str(el))
                 else:
-                    objOfDfs[str(groupName)] = y
+                    if addNewCol:
+                        objOfDfs[groupName] = filterAndConvertScheduleDataFrames(y, True, newColName, el)
+                    else:
+                        objOfDfs[groupName] = y
+
+    except Exception as e:
+        msgText = handleErrorMsg('\nError while filtering the single owners group list and concatenating the DataFrames.', getTraceback(e))
                     
+    if msgText: print(msgText)
     
     return objOfDfs
